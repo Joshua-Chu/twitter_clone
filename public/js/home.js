1763e5db2d7f7b9caab1748cae9e12dcc8b4f990
Joshua-Chu <jpchu@up.edu.ph>
2021-06-19 14:23:24 +0800

    outputting user posts

---
+++ public/js/home.js
@@ -0,0 +1,21 @@
+import { CreatePostHTML } from "./app.js";
+
+const getPosts = async () => {
+	const posts = await fetch("/api/posts");
+	const result = await posts.json();
+	outputPosts(result, $("#postContainer"));
+};
+
+const outputPosts = (posts, container) => {
+	container.html("");
+
+	if (posts.length === 0) {
+		container.append(`<span class="noResults">No Results Found...</span>`);
+	}
+	posts.forEach((post) => {
+		const html = CreatePostHTML(post);
+		container.append(html);
+	});
+};
+
+getPosts();
--- src/routes/api/posts.js
+++ src/routes/api/posts.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
 const Post = require("../../models/PostSchema");
 const User = require("../../models/UserSchema");
@@ -24,6 +25,24 @@ router.post("/", async (req, res) => {
 			.catch((error) => res.status(400).json({ errorMessage: error }));
 	} catch (error) {
 		res.status(500).send({ errorMessage: "Internal Server Error" });
+	}
+});
+
+router.get("/", (req, res) => {
+	const id = mongoose.Types.ObjectId(req.session.user._id);
+
+	try {
+		Post.find({ postedBy: id })
+			.populate("postedBy")
+			.sort({ createdAt: -1 })
+			.then((postData) => {
+				res.status(200).send(postData);
+			})
+			.catch((error) => {
+				res.status(400).send(error);
+			});
+	} catch (error) {
+		res.status(500).send(error);
 	}
 });
 
